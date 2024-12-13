8d505fe819afc922b07247fb05e1fb4739bdc7dc
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2024-12-14 00:36:45 +0200

    Add CORS support to backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const database = require("./database/crudrepository");
 const router = require("./routes/router");
+var cors = require('cors')
 
+
+app.use(cors())
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use('/api', router);
