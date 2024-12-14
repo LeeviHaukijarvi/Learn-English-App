d45fbdafb1508604354d94b06e1dba83aaf6f522
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2024-12-14 21:59:52 +0200

    Add CORS support, update start script for watch mode, and include react-router-dom dependency

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 5000;
 const path = require('path');
 const database = require("./database/crudrepository");
 const router = require("./routes/router");
+const cors = require('cors');
 
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
