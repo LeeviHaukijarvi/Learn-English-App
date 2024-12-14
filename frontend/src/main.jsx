eef0b41ed3cd51852ba30e17fabdb31b1572f899
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2024-12-14 20:25:52 +0200

    Implement routing with React Router and add Root component for navigation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Root from "./routes/root";
+import Learn from "./Learn";
+import ParentControl from "./ParentControl";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Learn />,
+      },
+      {
+        path: "/Learn",
+        element: <Learn />,
+      },
+      {
+        path: "parent-control",
+        element: <ParentControl />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontend/src/routes/root.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+
+export default function Root() {
+    return (
+        <>
+            <div id="Router">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href={`/parent-control`}>Parent Constrols</a>
+                        </li>
+                        <li>
+                            <a href={`/Learn`}>Learn</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="detail">
+                <Outlet />
+            </div>
+        </>
+    );
+}
