e727b3989435dfe74f51572d53cb10b4a0738465
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2025-01-09 19:01:30 +0200

    Implement authentication-based navigation and logout functionality in Root component

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,17 +1,50 @@
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useAuth } from "../AuthProvider";
 
 export default function Root() {
+    const auth = useAuth();
+
+    if (auth.token) {
+        return (
+            <>
+                <div id="Router">
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to={`/parent-control`}>Parent Control</Link>
+                            </li>
+                            <li>
+                                <Link to={`/learn`}>Learn</Link>
+                            </li>
+                            <li>
+                                <button
+                                    onClick={auth.logOut}>Logout
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div id="detail">
+                    <Outlet />
+                </div>
+            </>
+        );
+    }
+
     return (
         <>
             <div id="Router">
                 <nav>
                     <ul>
                         <li>
-                            <Link to={`/parent-control`}>Parent Control</Link>
+                            <Link to={`/learn`}>Learn</Link>
                         </li>
                         <li>
-                            <Link to={`/learn`}>Learn</Link>
+                            <Link to={`/login`}>Login</Link>
+                        </li>
+                        <li>
+                            <Link to={`/register`}>Register</Link>
                         </li>
                     </ul>
                 </nav>
