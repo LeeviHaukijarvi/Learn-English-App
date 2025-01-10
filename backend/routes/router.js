2f997ccc00758498bb096d024cf14fed2f6c5122
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2025-01-10 17:14:34 +0200

    Add endpoints for fetching and managing tags in the API

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -28,6 +28,15 @@ router.get("/users", async (req, res) => {
 
 })
 
+router.get("/tags", async (req, res) => {
+    try {
+        const tags = await database.getTags();
+        return res.status(200).json(tags)
+    } catch (err) {
+        return res.status(500).json({ error: "Failed to fetch tags", details: err.message });
+    }
+})
+
 router.post("/register", async (req, res) => {
     const { username, password } = req.body;
 
@@ -69,11 +78,11 @@ router.post("/login", async (req, res) => {
 
 
 router.post("/", async (req, res) => {
-    const { finnishWord, englishWord } = req.body;
+    const { finnishWord, englishWord, tag } = req.body;
 
     try {
-        await database.insertFinnishAndEnglish(finnishWord, englishWord)
-        res.status(201).json({ message: "Word added successfully" });
+        await database.insertFinnishAndEnglish(finnishWord, englishWord, tag)
+        res.status(201).json({ message: "Words added successfully" });
 
     } catch (err) {
         console.error(err)
@@ -91,15 +100,28 @@ router.delete("/:id", async (req, res) => {
     try {
         const result = await database.deleteWord(id);
         if (result === 0) {
-            return res.status(404).json({ error: "No word found with the provided ID" });
+            return res.status(404).json({ error: "No words found with the provided ID" });
         }
         return res.status(204).send();
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json({ error: "Failed to delete word", details: err.message });
+        return res.status(500).json({ error: "Failed to delete words", details: err.message });
     }
 })
+
+router.post("/tags", async (req, res) => {
+    const { tag } = req.body;
+
+    try {
+        await database.insertTag(tag)
+        res.status(201).json({ message: "Tag added successfully" });
+
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ error: err });
+    }
+});
 
 router.put("/:id", async (req, res) => {
     const id = parseInt(req.params.id);
@@ -107,6 +129,18 @@ router.put("/:id", async (req, res) => {
     try {
         await database.updateWords(finnishWord, englishWord, id);
         res.status(200).json({ message: "Word updated successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({ error: err.message });
+    }
+});
+
+router.put("/tag/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const { tag } = req.body;
+    try {
+        await database.updateWordsTag(tag, id);
+        res.status(200).json({ message: "Tag updated successfully" });
     } catch (err) {
         console.error(err);
         res.status(400).json({ error: err.message });
