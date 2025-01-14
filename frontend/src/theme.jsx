09be65cff78d826e93852c5024dbe7fbcab13201
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2025-01-14 23:45:08 +0200

    Update theme colors for primary and secondary palettes

--- frontend/src/theme.jsx
+++ frontend/src/theme.jsx
@@ -3,10 +3,10 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#ff0000',
+            main: '#00000'
         },
         secondary: {
-            main: '#00ff00',
+            main: '#38b38e',
         },
     },
 
