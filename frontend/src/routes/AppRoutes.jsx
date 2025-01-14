1742d652118c6547aa8fc77913625f57e4f0989e
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2025-01-14 19:00:29 +0200

    Moved from main to be more clear

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Root from './root';
+import Learn from '../Learn';
+import Login from '../Login';
+import ParentControl from '../ParentControl';
+import PrivateRoute from './privateRoute';
+import Register from '../Register';
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Root />}>
+                <Route index element={<Learn />} />
+                <Route path="login" element={<Login />} />
+                <Route path="parent-control" element={<PrivateRoute />}>
+                    <Route index element={<ParentControl />} />
+                </Route>
+                <Route path="learn" element={<Learn />} />
+                <Route path="*" element={<h1>404 error: Not Found</h1>} />
+                <Route path="register" element={<Register />} />
+            </Route>
+        </Routes>
+    );
+};
+
