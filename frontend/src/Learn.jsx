b292d6728ce761f2e045dae29d9ec42583f6a47f
Leevi Haukijärvi <leevi.haukijarvi@tuni.fi>
2025-01-14 18:57:14 +0200

    Refactor Learn component to enhance UI with Material-UI

--- frontend/src/Learn.jsx
+++ frontend/src/Learn.jsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import './stylesheets/Learn.css';
 import { useRef } from "react";
 import { fetchWords, fetchTags} from "./apiUtil";
+import { Container, MenuItem, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
+
+
 
 function Learn() {
   const [words, setWords] = useState([]);
@@ -13,7 +23,7 @@ function Learn() {
   const [points, setPoints] = useState(0);
   const [disabledInputs, setDisabledInputs] = useState({});
   const [tags, setTags] = useState([]);
-
+  const [selectedTagId, setSelectedTagId] = useState('');
 
 
   useEffect(() => {
@@ -81,90 +91,167 @@ function Learn() {
       if (language === "finnish") {
         toBeTranslated = word.finnish_word;
         translated = word.english_word;
-        placeholder = "In English";
       } else if (language === "english") {
         toBeTranslated = word.english_word;
         translated = word.finnish_word;
-        placeholder = "Suomeksi";
       }
 
       return (
         <div key={index}>
-          <p>{toBeTranslated}</p>
-          <input
-            ref={(ref) => (inputRefs.current[index] = ref)} // Set the unique ref for each input
-            type="text"
-            placeholder={placeholder}
-            value={inputValues[index] || ""}
-            style={{ backgroundColor: inputColors[index]}}
-            disabled={disabledInputs[index]}
-            onChange={(e) => {
-              const value = e.target.value;
-              setInputValues((prevValues) => ({
-                ...prevValues,
-                [index]: value,
-              }));
-              // Reset background color when user types
-              changeColor(index, "");
-            }}
-          />
-          <button
-            onClick={() => {
-              // Check if the input value matches the English word
-              // If it does, set the background color to green, otherwise red
-              const inputValue = inputRefs.current[index].value;
-              const formattedValue = inputValue.trim().toLowerCase();
-              if (formattedValue === translated) {
-                setPoints(points + 1);
-                changeColor(index, "green");
-                setDisabledInputs((prevDisabled) => ({
-                  ...prevDisabled,
-                  [index]: true,
-                }));
-              } else {
-                changeColor(index, "red");
-              }
-            }}
 
-            // Disable the button if the input is correct
-            disabled={disabledInputs[index]}
-          >
-            Check
-          </button>
+            <Paper elevation={3}
+                  sx={{ p: 2 }}
+                >
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                  <Grid>
+                    <Typography variant='h3'
+                      sx={{ m: 1, textAlign: "center", color: "primary.main" }}
+                    >
+                      {toBeTranslated}
+                    </Typography>
+                  </Grid>
+
+                  <Grid>
+                    <input
+                      ref={(ref) => (inputRefs.current[index] = ref)} // Set the unique ref for each input
+                      type="text"
+                      placeholder={placeholder}
+                      value={inputValues[index] || ""}
+                      style={{ backgroundColor: inputColors[index]}}
+                      disabled={disabledInputs[index]}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          [index]: value,
+                        }));
+                        // Reset background color when user types
+                        changeColor(index, "");
+                      }}
+                    />
+                  </Grid>
+
+                  <Grid>
+                    <Button
+                      sx={{ mt: 1 }}
+                      onClick={() => {
+                        // Check if the input value matches the English word
+                        // If it does, set the background color to green, otherwise red
+                        const inputValue = inputRefs.current[index].value;
+                        const formattedValue = inputValue.trim().toLowerCase();
+                        if (formattedValue === translated) {
+                          setPoints(points + 1);
+                          changeColor(index, "green");
+                          setDisabledInputs((prevDisabled) => ({
+                            ...prevDisabled,
+                            [index]: true,
+                          }));
+                        } else {
+                          changeColor(index, "red");
+                        }
+                      }}
+                      // Disable the button if the input is correct
+                      disabled={disabledInputs[index]}
+                    >
+                      Check
+                    </Button>
+                  </Grid>
+                </Box>
+              </Paper>
         </div>
       );
     });
   }
 
   return (
-    <>
-      <h1>Learn English</h1>
-      <p>Points: {points}</p>
-      <select
-        onChange={(e) => {
-          const selectedTagId = parseInt(e.target.value);
-          changeWordsByTag(selectedTagId);
+    <Container>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <option value="">No tag selected</option>
-        {tags.map((tag) => (
-          <option key={tag.id} value={tag.id}>
-            {tag.tag}
-          </option>
-        ))}
-      </select>
-      {listWords(words, language)}
-      <button onClick={handleRefresh}>Clear</button>
-      <button onClick={() => {
-        if (language === "finnish") {
-          setLanguage("english");
-        } else if (language === "english") {
-          setLanguage("finnish");
-        }
-        handleRefresh();
-      }}>Flip</button>
-    </>
+        <Typography variant="h1" sx={{ my: 4, textAlign: "center", color: "primary.main" }}>
+          Learn English
+        </Typography>
+        <Typography variant='h3'>
+          Points: {points}
+        </Typography>
+
+        <Box>
+          <FormControl sx={{ m: 3, minWidth: 80 }}>
+            <InputLabel id="tag-select">Tag</InputLabel>
+            <Select
+              onChange={(e) => {
+                const tagId = parseInt(e.target.value);
+                changeWordsByTag(tagId);
+                setSelectedTagId(tagId);
+              }}
+              autoWidth
+              label="Tag"
+              value={selectedTagId}
+            >
+              <MenuItem value={0}>All</MenuItem>
+              {tags.map((tag) => (
+                <MenuItem key={tag.id} value={tag.id}>
+                  {tag.tag}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+          <Grid container spacing={2}
+            sx={{justifyContent: 'center'}}
+          >
+            {listWords(words, language)}
+          </Grid>
+      </Box>
+      <Box
+        sx={{
+
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Button
+          onClick={handleRefresh}
+          sx={{ mr: 1 }}
+          variant="contained"
+          color="secondary"
+        >
+          Clear
+        </Button>
+          <Button
+            onClick={() => {
+              if (language === "finnish") {
+                setLanguage("english");
+              } else if (language === "english") {
+                setLanguage("finnish");
+              }
+              handleRefresh();
+            }}
+            variant="contained"
+            color="secondary"
+            sx={{ m: 2 }}
+          >
+            Flip
+          </Button>
+      </Box>
+    </Container>
   );
 }
+
 
 export default Learn;
